b2ab8159f85c69027a57e30d40ea35be0040d0a2
seunggyu <seunggyu97@gmail.com>
2023-02-26 14:42:01 +0900

    #5 : 로그인버튼 ui 구현, 신선고 이름 로고 삽입

--- app/src/main/java/com/sinsungo/android/ui/LoginActivity.kt
+++ app/src/main/java/com/sinsungo/android/ui/LoginActivity.kt
@@ -6,17 +6,19 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.sinsungo.android.MainActivity
+import com.sinsungo.android.R
+import com.sinsungo.android.databinding.ActivityLoginBinding
 import com.sinsungo.android.databinding.ActivitySplashBinding
 import com.sinsungo.android.viewModel.SplashViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class SplashActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySplashBinding
-    private val splashViewModel: SplashViewModel by viewModels()
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,13 +27,13 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setContentView(binding.root)
 
         // 2초 지연
         lifecycleScope.launch {
             delay(2000)
-            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+            startActivity(Intent(this@LoginActivity, LoginActivity::class.java))
             finish()
         }
     }
