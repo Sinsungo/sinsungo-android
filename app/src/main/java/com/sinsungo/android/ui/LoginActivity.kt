b8143888c42ef2c74aa71fdfb964dd0347e40095
seunggyu <seunggyu97@gmail.com>
2023-02-26 19:26:55 +0900

    #7 : 소셜 로그인 버튼 애니메이션 적용

--- app/src/main/java/com/sinsungo/android/ui/LoginActivity.kt
+++ app/src/main/java/com/sinsungo/android/ui/LoginActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -14,6 +17,8 @@ import com.sinsungo.android.R
 import com.sinsungo.android.databinding.ActivityLoginBinding
 import com.sinsungo.android.databinding.ActivitySplashBinding
 import com.sinsungo.android.viewModel.SplashViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -28,13 +33,25 @@ class LoginActivity : AppCompatActivity() {
 
     private fun init() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        setContentView(binding.root)
 
-        // 2초 지연
+        // anim 하나만으로 적용하면 이상하게 끊기는 이유를 모르겠습니다,,
+        val anim = AnimationUtils.loadAnimation(this, R.anim.anim_translate_right)
+        val anim2 = AnimationUtils.loadAnimation(this, R.anim.anim_translate_right)
+        val anim3 = AnimationUtils.loadAnimation(this, R.anim.anim_translate_right)
         lifecycleScope.launch {
-            delay(2000)
-            startActivity(Intent(this@LoginActivity, LoginActivity::class.java))
-            finish()
+            with(binding) {
+                delay(100)
+                btnKakaoLogin.startAnimation(anim)
+                btnKakaoLogin.isVisible = true
+
+                delay(100)
+                binding.btnGoogleLogin.startAnimation(anim2)
+                btnGoogleLogin.isVisible = true
+
+                delay(100)
+                binding.btnAppleLogin.startAnimation(anim3)
+                btnAppleLogin.isVisible = true
+            }
         }
     }
 }
